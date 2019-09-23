52133e63e302fe79e11ca3e4c789d5c650cb5133
KhelifaSaifEddine <50869378+KhelifaSaifEddine@users.noreply.github.com>
2019-09-24 00:47:31 +0200

    Add files via upload

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using FFImageLoading.Forms.Platform;
+
+namespace MasterDetailPageNavigation.Droid
+{
+    [Activity(Label = "MasterDetailPageNavigation",Icon ="@drawable/logo.png" ,  Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            Rg.Plugins.Popup.Popup.Init(this, savedInstanceState);
+            Xamarin.FormsMaps.Init(this, savedInstanceState);
+            CachedImageRenderer.Init(true);
+            Plugin.CurrentActivity.CrossCurrentActivity.Current.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+
+        }
+    }
+}
